128190015ea7de44616274ebcc60e729cbad5fda
naoya-imai <imai-naoya476@g.ecc.u-tokyo.ac.jp>
2022-06-15 12:18:55 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import db from "./firebase";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  // browseがマウントされたときに一回だけuseEffectの中身が実行される
+  useEffect(() => {
+    // データベースからデータを取得する
+    const postData = collection(db, "posts");
+    console.log(postData);
+    // const docSnap = getDocs(postData);
+    // console.log(docSnap);
+    getDocs(postData).then((snapShot) => {
+      // console.log(snapShot.docs.map((doc) => doc.data()));
+      // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      console.log(posts);
+    });
+    // getDoc(postData).then((snapShot) => {
+    //   console.log(snapShot);
+    // });
+
+    /* リアルタイムで取得 */
+    onSnapshot(postData, (post) => {
+      setPosts(post.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {posts.map((post) => (
+          <div key={post.title}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
